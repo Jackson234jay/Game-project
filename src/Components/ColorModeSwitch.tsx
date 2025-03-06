a2924baadd9a4663842132544dd9aa564a1ec275
Johnezekiel <blossomjohn399@gmail.com>
2025-03-06 19:30:56 +0100

    Color Mode Switch Update

--- src/Components/ColorModeSwitch.tsx
+++ src/Components/ColorModeSwitch.tsx
@@ -1,4 +1,4 @@
-import { HStack, IconButton, Switch, Text } from "@chakra-ui/react";
+import { HStack, IconButton, Text } from "@chakra-ui/react";
 import { useColorMode } from "@/components/ui/color-mode";
 import { LuMoon, LuSun } from "react-icons/lu";
 
@@ -10,7 +10,7 @@ const ColorModeSwitch = () => {
         <IconButton onClick={toggleColorMode} variant={"outline"} size={"sm"}>
           {colorMode === "dark" ? <LuMoon /> : <LuSun />}
         </IconButton>
-        <Text>Dark Mode</Text>
+        <Text>{colorMode === "dark" ? "Dark Mode" : "Light Mode"}</Text>
       </HStack>
     </div>
   );

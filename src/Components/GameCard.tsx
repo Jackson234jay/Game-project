71a6b2c1ff3d8b140cbf0338208937d4a78899b0
Johnezekiel <blossomjohn399@gmail.com>
2025-03-07 19:31:12 +0100

    Show loading skeletons

--- src/Components/GameCard.tsx
+++ src/Components/GameCard.tsx
@@ -1,5 +1,12 @@
 import { Game } from "@/hooks/useGames";
-import { Card, HStack, Image, Text } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  HStack,
+  Image,
+  Text,
+} from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "@/services/image-url";
@@ -10,17 +17,21 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card.Root borderRadius={10} overflow={"hidden"}>
+    <Card.Root maxW={"300px"} borderRadius={10} overflow="hidden">
       <Image src={getCroppedImageUrl(game.background_image)} />
-      <Card.Body>
-        <Card.Header fontSize={"2xl"}>{game.name}</Card.Header>
+      <CardHeader>
+        <Text fontSize="2xl" fontWeight="bold">
+          {game.name}
+        </Text>
+      </CardHeader>
+      <CardBody>
         <HStack justifyContent="space-between">
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
-      </Card.Body>
+      </CardBody>
     </Card.Root>
   );
 };
--- src/Components/GameGrid.tsx
+++ src/Components/GameGrid.tsx
@@ -1,23 +1,29 @@
 import useGames from "@/hooks/useGames";
-import { SimpleGrid, Text } from "@chakra-ui/react";
+import { SimpleGrid, Text, Box } from "@chakra-ui/react";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
-  const { games, error } = useGames();
+  const { games, error, isLoading } = useGames();
+  const Skeleton = [1, 2, 3, 4, 5, 6];
+
   return (
-    <>
-      {error && <Text>{error}</Text>}
+    <Box display="flex" justifyContent="center">
+      {error && <Text color="red.500">{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        padding={"10px"}
-        spaceX={10}
-        spaceY={10}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        padding="10px"
+        gap={6}
+        justifyItems="center" // ✅ Centers cards inside the grid
+        maxW="100%"
       >
+        {isLoading &&
+          Skeleton.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
         ))}
       </SimpleGrid>
-    </>
+    </Box>
   );
 };
 

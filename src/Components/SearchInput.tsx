d22df89773c20cfd1572a3028b0c65863eccf50a
Johnezekiel <blossomjohn399@gmail.com>
2025-03-11 19:37:10 +0100

    Implement Searching

--- src/Components/SearchInput.tsx
+++ src/Components/SearchInput.tsx
@@ -1,16 +1,30 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 
-const SearchInput = () => {
+interface Props {
+  onSearch: (search: string) => void;
+}
+
+const SearchInput = ({ onSearch }: Props) => {
+  const ref = useRef<HTMLInputElement>(null);
   return (
-    <InputGroup>
-      <InputLeftElement children={<BsSearch />} />
-      <Input
-        borderRadius={20}
-        placeholder="Search games..."
-        variant={"filled"}
-      />
-    </InputGroup>
+    <form
+      onChange={(event) => {
+        event.preventDefault();
+        if (ref.current) onSearch(ref.current.value);
+      }}
+    >
+      <InputGroup>
+        <InputLeftElement children={<BsSearch />} />
+        <Input
+          borderRadius={20}
+          ref={ref}
+          placeholder="Search games..."
+          variant={"filled"}
+        />
+      </InputGroup>
+    </form>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
+import "../src/index.css";
 import theme from "./theme";
 import { ColorModeScript } from "@chakra-ui/react";
 
